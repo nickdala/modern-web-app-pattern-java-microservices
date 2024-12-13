ec665d7ca48a39fc16b5f71b082c8b807224530d
Nick Dalalelis <ndalalelis@microsoft.com>
2024-12-13 13:19:21 -0500

    Initial Support Guide service

---
+++ apps/supportguide/src/main/java/com/contoso/cams/supportguide/SupportguideApplication.java
@@ -0,0 +1,34 @@
+package com.contoso.cams.supportguide;
+
+import com.contoso.cams.protobuf.guides.v1.GuideUploadUrlRequest;
+import com.contoso.cams.protobuf.guides.v1.GuideUploadUrlResponse;
+import com.contoso.cams.protobuf.guides.v1.SupportGuideServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportguideApplication.class, args);
+	}
+}
+
+@GrpcService
+class SupportGuideService extends SupportGuideServiceGrpc.SupportGuideServiceImplBase {
+    @Override
+    public void getGuideUploadUrl(GuideUploadUrlRequest request, StreamObserver<GuideUploadUrlResponse> responseObserver) {
+        String fileName = request.getFileName();
+        String uploadUrl = "https://upload.example.com/" + fileName;
+        GuideUploadUrlResponse response = GuideUploadUrlResponse.newBuilder()
+                .setUploadUrl(uploadUrl)
+                .setFileName(fileName)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
+
+
